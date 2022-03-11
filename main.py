6867324c81a369b8e66ead2e652c193f8c076cb1
Q <fhsndxva@gmail.com>
2022-03-12 02:30:45 +0300

    Большая задача по Maps API. Часть №2

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import mapstatic as ms
 from PIL import Image
 
 from PyQt5 import uic
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
@@ -14,12 +15,25 @@ class MyWidget(QMainWindow):
         super().__init__()
         uic.loadUi('main.ui', self)
 
-        Image.open(ms.get_img({'ll': '0.0,0.0', 'z': '16'})).save('map.png')
+        self.z = 16
+
+        self.update_img()
+
+    def update_img(self):
+        Image.open(ms.get_img({'ll': '20.0,20.0', 'z': self.z})).save('map.png')
 
         pixmap = QPixmap('map.png')
 
         self.img.setFixedSize(pixmap.width(), pixmap.height())
         self.img.setPixmap(pixmap)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Up:
+            self.z = min(self.z + 1, 17)
+        elif event.key() == Qt.Key_Down:
+            self.z = max(self.z - 1, 0)
+
+        self.update_img()
 
 
 if __name__ == '__main__':
