712fd62bc71a2512fc592060a7e9a8759451aa2b
Q <fhsndxva@gmail.com>
2022-03-12 23:20:05 +0300

    Большая задача по Maps API. Часть №5-7

---
+++ scalmap.py
@@ -0,0 +1,14 @@
+import requests
+
+
+def selection_scale(response: requests.models.Response):
+    # Преобразуем ответ в json-объект
+    json_response = response.json()
+
+    # Получаем первый топоним из ответа геокодера.
+    toponym = json_response["response"]["GeoObjectCollection"][
+        "featureMember"][0]["GeoObject"]
+    # Координаты центра топонима:
+    toponym_coodrinates = toponym["Point"]["pos"]
+
+    return toponym_coodrinates.split(" ")  # Долгота и широта
